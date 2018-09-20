4958be9d7ac6f5943852bf030217d6ca73fc4fd5
Atuski <1763239924@qq.com>
2018-09-20 19:46:57 +0800

    debug

--- src/components/layouts.vue
+++ src/components/layouts.vue
@@ -1,13 +1,18 @@
 <template>
   <section class="container" > <!--最外层容器-->
     <section class="menu"> <!--左边的容器-->
+    	<menus></menus>
     </section>
     <section class="content-container"><!--右边的容器-->
     </section>
   </section>
 </template>
 <script>
-  export  default {};
+	import menus from './menus';
+  export  default {
+  	components: { // * 注册menus组件，让其可以在template调用
+    menus,todo
+  };
 </script>
 <style lang="less">
 @import '../common/style/layouts.less';
